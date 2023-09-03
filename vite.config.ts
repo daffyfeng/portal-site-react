58e741d77e42fd0665cc7560ebb8f7b70abd06db
daffyfeng <big_feng@foxmail.com>
2023-09-03 11:35:41 +0800

    引入antd并按需引入&路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, 'src'),
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ],
+    })
+  ],
+  resolve: {
+    alias: {
+      "~": path.join(__dirname, './'),
+      "@": path.join(__dirname, './src'),
+    }
+  }
 })
