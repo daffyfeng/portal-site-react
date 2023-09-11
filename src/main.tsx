d71d8414586e7cb31f20b33170a0fb44ffb9bba1
daffyfeng <big_feng@foxmail.com>
2023-09-11 15:40:19 +0800

    路由改

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "reset-css"
-import "@/assets/scss/global.scss"
-import { Router } from '@/router'
-import { RouterProvider } from 'react-router-dom'
-import { RouterBeforeEach } from "@/utils/useUtilsNavigate";
-RouterBeforeEach((to, from) => {
-  console.log("路由守卫to", to)
-  console.log("路由守卫from", from)
-  return true;
-})
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'reset-css';
+import '@/assets/scss/global.scss';
+import { Router } from '@/router';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={Router()} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,24 @@
-import { Suspense, lazy } from "react"
-import { Navigate, createBrowserRouter, redirect, useRouteError } from 'react-router-dom'
+import { Suspense, lazy } from 'react';
+import {
+  Navigate,
+  NavigateOptions,
+  To,
+  createBrowserRouter,
+  redirect,
+  useRouteError,
+} from 'react-router-dom';
 // import Home from "@/views/Home"
-import Login from "@/views/login"
+import Login from '@/views/login';
 
-const HomeLayout = lazy(() => import('@/views/HomeLayout'))
-const Home = lazy(() => import('@/views/Home'))
+const HomeLayout = lazy(() => import('@/views/HomeLayout'));
+const Home = lazy(() => import('@/views/Home'));
+
+type IrouterBeforeLoad = (to: Ito, location?: Location) => Response | null;
+
+interface Ito {
+  toPath: To;
+  options?: NavigateOptions;
+}
 
 // const withLoadingComponent = (comp: JSX.Element) => {
 //     return (
@@ -14,75 +28,75 @@ const Home = lazy(() => import('@/views/Home'))
 //     )
 // }
 
-
-type IRouterBeforeLoad = (res: any, redirectUrl: string) => boolean;
-let routerLoader: IRouterBeforeLoad;
-const _redirectUrl: string = "/";
-
-
 const routes = [
-    {
-        path: '/',
-        element: (<Navigate to="/index"></Navigate>) // 默认进入子路由中的首页，为了让HomeLayout能够一直加载
-    },
-    {
-        path: '/',
-        element: <HomeLayout />,
-        children: [
-            {
-                path: '/index',
-                // element: withLoadingComponent(<Home />),
-                component: Home
-            }
-        ]
-
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
-]
+  {
+    path: '/',
+    element: <Navigate to='/index'></Navigate>, // 默认进入子路由中的首页，为了让HomeLayout能够一直加载
+  },
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        path: '/index',
+        // element: withLoadingComponent(<Home />),
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+];
 
 function ErrorBoundary() {
-    const error: any = useRouteError();
-    return <div>
-        <div>{error.message}</div>
-        <div>{error.stack}</div>
-    </div>;
+  const error: any = useRouteError();
+  return (
+    <div>
+      <div>{error.message}</div>
+      <div>{error.stack}</div>
+    </div>
+  );
 }
 
 // 路由处理方式
 const generateRouter = (routers: any) => {
-    return routers.map((item: any) => {
-        if (item.children) {
-            item.children = generateRouter(item.children)
-        }
-        item.element = item.element || <Suspense fallback={
-            <div>加载中...</div>
-        }>
-            {/* 把懒加载的异步路由变成组件装载进去 */}
-            {/* <KeepAlive id={item.name} cacheKey={item.name}> */}
-            <item.component />
-            {/* </KeepAlive> */}
-        </Suspense>
-        item.errorElement = <ErrorBoundary></ErrorBoundary>
-        item.loader = async (res: any) => {
-            if (routerLoader && !item.children) {
-                if (routerLoader(res, _redirectUrl)) {
-                    return res;
-                } else {
-                    return redirect(_redirectUrl);
-                }
-            }
-            return res;
-        }
-        return item
-    })
-}
+  return routers.map((item: any) => {
+    if (item.children) {
+      item.children = generateRouter(item.children);
+    }
+    item.element = item.element || (
+      <Suspense fallback={<div>加载中...</div>}>
+        {/* 把懒加载的异步路由变成组件装载进去 */}
+        {/* <KeepAlive id={item.name} cacheKey={item.name}> */}
+        <item.component />
+        {/* </KeepAlive> */}
+      </Suspense>
+    );
+    item.errorElement = <ErrorBoundary></ErrorBoundary>;
+    item.loader = async (res: any) => {
+      console.log(res);
+      const url = new URL(res.request.url);
+      const result = beforeRouterLoader({ toPath: url.pathname });
 
-const RouterLoader = (fun: IRouterBeforeLoad) => {
-    routerLoader = fun;
-}
+      return result || res;
+    };
+    return item;
+  });
+};
 
-const Router = () => createBrowserRouter(generateRouter([...routes]))
-export { Router, RouterLoader }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const beforeRouterLoader: IrouterBeforeLoad = (to, from) => {
+  if (to.toPath === '/login') {
+    localStorage.removeItem('satoken');
+  } else {
+    if (!localStorage.getItem('satoken')) {
+      return redirect('/login');
+    }
+  }
+  return null;
+};
+
+const Router = () => createBrowserRouter(generateRouter([...routes]));
+export { Router };
